416ac2d86499f7b9c69d8e18ac11521fb0f9c9c1
LuisSkap <118025076+LuisSkap@users.noreply.github.com>
2023-07-06 15:45:55 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import express from 'express';
-import {dirname, join} from 'path';
-import {fileURLToPath} from 'url';
+// import express from 'express';
+// import {dirname, join} from 'path';
+// import {fileURLToPath} from 'url';
+
+const express = require('express');
+const path = require('path');
+//const fileURLToPath = require('url');
 
 const app = express()
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+//const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.set('views', join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// console.log(join(__dirname, 'public'))
 
 app.listen(3000)
 console.log('Server is listening on port: ', 3000)
